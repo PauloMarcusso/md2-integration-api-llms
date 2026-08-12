e129e058b8f24afac5f78786fdf0cab472d1bbbb
paulo henrique <phmarcusso1@gmail.com>
2026-08-11 22:13:49 -0300

    connecting to open router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import {createServer} from "./server.ts"
+import { config } from "./config.ts";
+import { OpenRouterService } from "./openrouterService.ts";
+import { createServer } from "./server.ts";
 
-const app = createServer()
+const routerService = new OpenRouterService(config)
+const app = createServer(routerService)
 
-await app.listen({port: 3000, host: '0.0.0.0'})
+await app.listen({ port: 3000, host: '0.0.0.0'})
+console.log('server running at 3000')
 
-app.inject({
-    method: 'POST',
-    url: '/chat',
-    body:{question:'Hello world'}
-}).then((response) => {
-    console.log('Response Status', response.statusCode)
-    console.log('Response body', response.body)
-})
+// app.inject({
+//     method: 'POST',
+//     url: '/chat',
+//     body:{ question:'What is rate limiting?'}
+// }).then((response) => {
+//     console.log('Response status', response.statusCode)
+//     console.log('Response body', response.body)
+// })
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
 import Fastify from "fastify";
+import { OpenRouterService } from "./openrouterService.ts";
 
-export const createServer = ( ) => {
-    const app = Fastify({logger: false})
+export const createServer = (routerService: OpenRouterService ) => {
+    const app = Fastify({ logger: false })
 
     app.post('/chat', {
         schema: {
             body: {
-                type : 'object',
+                type: 'object',
                 required: ['question'],
-                properties: {
-                    question: {type: 'string', minLength: 5}
+                properties:  {
+                    question: { type: 'string', minLength: 5}
                 }
             }
         }
     }, async (request, reply) => {
         try {
+
             const { question } = request.body as { question: string }
-            return reply.send('hello!')
+            const response = await routerService.generate(question)
+            return reply.send(response)
         } catch (error) {
             console.error('Error handling /chat request:', error)
             return reply.code(500)
